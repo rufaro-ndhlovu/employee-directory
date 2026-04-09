3058aacb05630a8f625696ed15ea12ed852e384a
Rufaro <80164120+rufaro-ndhlovu@users.noreply.github.com>
2026-04-09 13:48:09 +0100

    Feat: update firebase and related dependencies to latest versions

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -8,6 +8,10 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log("ENV CHECK:", {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+});
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
