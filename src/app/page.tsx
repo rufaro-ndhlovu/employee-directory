82dc2d54f810f705e43a90aa937e901e142542e6
Rufaro <80164120+rufaro-ndhlovu@users.noreply.github.com>
2026-04-11 14:10:37 +0100

    Feat: updated the root page style to macth the login and register pages, colour and styling. Added comments to the login and root css files.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 import styles from "./page.module.css";
 import Image from "next/image";
-
-import Button from "react-bootstrap/Button";
 import { useRouter } from "next/navigation";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import ButtonComp from "@/components/components/button";
 
 export default function Home() {
   const router = useRouter();
@@ -15,25 +14,39 @@ export default function Home() {
       <div className={styles.overlay}></div>
 
       <main className={styles.main}>
-        {/*Logo */}
-        <Image src="/logo2.png" alt="logo" width={310} height={120} />
-        {/*--------- Slogan -------------- */}
-        <p>Search, update, and manage employees in one place.</p>
+        <div className={styles.content}>
+          {/*Logo */}
+          <div className={styles.image}>
+            <img src="/logo3nobg.png" alt="logo" width={420} height={150} />
+          </div>
+          <br />
+          {/*--------- Slogan -------------- */}
+          <h4>Search, update, and manage employees in one place.</h4>
+          <br />
 
-        {/*--------- Login and Sign up buttons -------------- */}
-        <div className={styles.buttonsContainer}>
-          <Button
-            className={styles.buttons}
-            onClick={() => router.push("/login")}
-          >
-            Login
-          </Button>
-          <Button
-            className={styles.buttons}
-            onClick={() => router.push("/signUp")}
-          >
-            Sign Up
-          </Button>
+          {/*--------- Login and Sign up buttons -------------- */}
+          <div className={styles.buttonsContainer}>
+            <ButtonComp
+              text="Sign In"
+              onClick={() => router.push("/login")}
+              style={{
+                width: "12rem",
+                height: "3rem",
+                color: "#fff",
+                background: "linear-gradient(135deg, #6fc7c2, #a185ff)",
+              }}
+            />
+            <ButtonComp
+              text="Sign Up"
+              onClick={() => router.push("/signUp")}
+              style={{
+                width: "12rem",
+                height: "3rem",
+                color: "#fff",
+                background: "linear-gradient(135deg, #6fc7c2, #a185ff)",
+              }}
+            />
+          </div>
         </div>
       </main>
       <footer className={styles.footer}></footer>
